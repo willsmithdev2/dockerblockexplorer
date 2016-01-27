e54223d9d03ac80e45278ce11c495bbbf4fb14b2
vagrant <vagrant@precise64.(none)>
2016-01-27 13:35:01 +0000

    Changed startup script

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #This env variable is needed to know the IP address of the boot2docker IP. May requre tinkering. 
-export DOCKER_IP="$(ifconfig | grep -A 1 'eth1' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+export DOCKER_IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-docker rm nginx
-docker rm consul
-docker rm registrator
-docker rm app1
-docker rm app2
+docker rm -f nginx
+docker rm -f consul
+docker rm -f registrator
+docker rm -f app1
+docker rm -f app2
 
 docker-compose up -d
 
