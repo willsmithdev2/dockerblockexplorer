14692084267adb186f8d82a198b218954a6262b6
Will Smith <willsmithdev@gmail.com>
2016-01-28 14:22:41 +0000

    Updating script to print contents of current dir

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,6 +2,11 @@
 #This env variable is needed to know the IP address of the boot2docker IP. May requre tinkering. 
 export DOCKER_IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
+
+pwd
+ls $pwd
+
+
 docker rm -f nginx
 docker rm -f consul
 docker rm -f registrator
